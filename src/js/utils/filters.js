5915c19ad8616a3dffa06fd03db80d65a2372997
Stanley <stanley.defer@hotmail.Fr>
2024-09-03 03:03:54 +0200

    🎨 Code formatting

--- src/js/utils/filters.js
+++ src/js/utils/filters.js
@@ -30,38 +30,20 @@ let displayedRecipes = recipesData
 
 // ------------ Event listeners ------------------ //
 ingredientBtn.addEventListener("click", () => {
-    isIngredientsOpen = toggleDropdownButton(
-        isIngredientsOpen,
-        ingredientDropdown,
-        document.querySelector(".chevron_ingredient"),
-    )
+    isIngredientsOpen = toggleDropdownButton(isIngredientsOpen, ingredientDropdown, document.querySelector(".chevron_ingredient"))
 })
 applianceBtn.addEventListener("click", () => {
-    isAppliancesOpen = toggleDropdownButton(
-        isAppliancesOpen,
-        applianceDropdown,
-        document.querySelector(".chevron_appliance"),
-    )
+    isAppliancesOpen = toggleDropdownButton(isAppliancesOpen, applianceDropdown, document.querySelector(".chevron_appliance"))
 })
 utensilBtn.addEventListener("click", () => {
-    isUtensilsOpen = toggleDropdownButton(
-        isUtensilsOpen,
-        utensilDropdown,
-        document.querySelector(".chevron_utensil"),
-    )
+    isUtensilsOpen = toggleDropdownButton(isUtensilsOpen, utensilDropdown, document.querySelector(".chevron_utensil"))
 })
 
-ingredientSearchInput.addEventListener("input", () =>
-    handleFilterInput(ingredientSearchInput, ingredientDropdown),
-)
-applianceSearchInput.addEventListener("input", () =>
-    handleFilterInput(applianceSearchInput, applianceDropdown),
-)
-utensilSearchInput.addEventListener("input", () =>
-    handleFilterInput(utensilSearchInput, utensilDropdown),
-)
-
 filtersContainer.addEventListener("click", deleteSelectedFilter)
+
+ingredientSearchInput.addEventListener("input", () => handleFilterInput(ingredientSearchInput, ingredientDropdown))
+applianceSearchInput.addEventListener("input", () => handleFilterInput(applianceSearchInput, applianceDropdown))
+utensilSearchInput.addEventListener("input", () => handleFilterInput(utensilSearchInput, utensilDropdown))
 
 searchbarInput.addEventListener("input", () => handleSearchbarInput())
 
@@ -98,80 +80,47 @@ function createAllFiltersList() {
 }
 
 function createIngredientsList() {
-    const ingredients = recipesData.flatMap((recette) =>
-        recette.ingredients.map((ing) => ing.ingredient.toLowerCase()),
-    )
-    handleFiltersListCreation(
-        ".ingredient_list",
-        ingredientDropdown,
-        ingredients,
-        "ingredients",
-    )
+    const ingredients = recipesData.flatMap((recette) => recette.ingredients.map((ing) => ing.ingredient.toLowerCase()))
+    handleFiltersListCreation(".ingredient_list", ingredientDropdown, ingredients, "ingredients")
 }
 
 function createAppliancesList() {
-    const appliances = recipesData.flatMap((recette) =>
-        recette.appliance.toLowerCase(),
-    )
-    handleFiltersListCreation(
-        ".appliance_list",
-        applianceDropdown,
-        appliances,
-        "appliance",
-    )
+    const appliances = recipesData.flatMap((recette) => recette.appliance.toLowerCase())
+    handleFiltersListCreation(".appliance_list", applianceDropdown, appliances, "appliance")
 }
 
 function createUtensilsList() {
-    const utensils = recipesData.flatMap((recette) =>
-        recette.ustensils.map((utensil) =>
-            utensil.replace(/[^a-zA-ZÀ-ÿ\s]/g, "").toLowerCase(),
-        ),
-    )
-    handleFiltersListCreation(
-        ".utensil_list",
-        utensilDropdown,
-        utensils,
-        "ustensils",
-    )
+    const utensils = recipesData.flatMap((recette) => recette.ustensils.map((utensil) => utensil.replace(/[^a-zA-ZÀ-ÿ\s]/g, "").toLowerCase()))
+    handleFiltersListCreation(".utensil_list", utensilDropdown, utensils, "ustensils")
 }
 
-function handleFiltersListCreation(
-    listContainerSelector,
-    dropdownContainer,
-    data,
-    filterType,
-) {
+function handleFiltersListCreation(listContainerSelector, dropdownContainer, data, filterType) {
     const listContainer = document.querySelector(listContainerSelector)
     let uniqueItems = []
     let recipesMatchingData = []
 
     // Get the data for the specific filter type
-
     switch (filterType) {
         case "ingredients":
             recipesMatchingData = data.filter((ingredient) =>
                 displayedRecipes.some((recipe) =>
-                    recipe.ingredients.some(
-                        (ing) => ing.ingredient.toLowerCase() === ingredient,
-                    ),
-                ),
+                    recipe.ingredients.some((ing) => ing.ingredient.toLowerCase() === ingredient)
+                )
             )
             break
         case "appliance":
             recipesMatchingData = data.filter((appliance) =>
-                displayedRecipes.some(
-                    (recipe) => recipe.appliance.toLowerCase() === appliance,
-                ),
+                displayedRecipes.some((recipe) =>
+                    recipe.appliance.toLowerCase() === appliance
+                )
             )
             break
 
         case "ustensils":
             recipesMatchingData = data.filter((utensil) =>
                 displayedRecipes.some((recipe) =>
-                    recipe.ustensils.some(
-                        (ustensil) => ustensil.toLowerCase() === utensil,
-                    ),
-                ),
+                    recipe.ustensils.some((ustensil) => ustensil.toLowerCase() === utensil)
+                )
             )
             break
         default:
@@ -179,23 +128,17 @@ function handleFiltersListCreation(
     }
 
     // Get unique items that are not in the active filters
-    uniqueItems = [
-        ...new Set(
-            recipesMatchingData.filter(
-                (item) => !activeFilters.includes(item.toLowerCase()),
-            ),
-        ),
-    ]
+    uniqueItems = [...new Set(recipesMatchingData.filter((item) =>
+        !activeFilters.includes(item.toLowerCase())
+    ))]
 
     uniqueItems.sort((a, b) => a.localeCompare(b))
 
     // Create the list items for the filter dropdown
-    listContainer.innerHTML = uniqueItems
-        .map((item) => {
-            const capitalizedItem = item.charAt(0).toUpperCase() + item.slice(1)
-            return `<li data-filter-type="${filterType}" class="block cursor-pointer py-2 px-4 hover:bg-yellow text-sm">${capitalizedItem}</li>`
-        })
-        .join("")
+    listContainer.innerHTML = uniqueItems.map((item) => {
+        const capitalizedItem = item.charAt(0).toUpperCase() + item.slice(1)
+        return `<li data-filter-type="${filterType}" class="block cursor-pointer py-2 px-4 hover:bg-yellow text-sm">${capitalizedItem}</li>`
+    }).join("")
 
     const filterItems = dropdownContainer.querySelectorAll("li")
     addListenersToFiltersList(filterItems)
@@ -231,41 +174,31 @@ function handleFilterClick(event) {
 }
 
 function cleanSearchInput(filterType) {
-    const inputElement = document.querySelector(
-        `input[data-filter-type="${filterType}"]`,
-    )
+    const inputElement = document.querySelector(`input[data-filter-type="${filterType}"]`)
     if (inputElement) {
         inputElement.value = ""
     }
 }
 
 function deleteSelectedFilter(event) {
-    if (
-        event.target.classList.contains("close_btn") ||
-        event.target.classList.contains("selected_filter_btn")
-    ) {
+    if (event.target.classList.contains("close_btn") || event.target.classList.contains("selected_filter_btn")) {
         const button = event.target.closest(".selected_filter_btn")
         const filter = button.textContent.trim().toLowerCase()
+
         // Remove the filter from active filters
         activeFilters = activeFilters.filter((element) => element !== filter)
         button.remove()
-        handleSearchbarInput()
         handleSearchbarInput()
     }
 }
 
 function filterRecipes(recipes, filters) {
     filteredRecipes = recipes.filter((recipe) =>
-        filters.every(
-            (filter) =>
-                recipe.ingredients.some(
-                    (item) => item.ingredient.toLowerCase() === filter,
-                ) ||
-                recipe.appliance.toLowerCase() === filter ||
-                recipe.ustensils.some(
-                    (ustensil) => ustensil.toLowerCase() === filter,
-                ),
-        ),
+        filters.every((filter) =>
+            recipe.ingredients.some((item) => item.ingredient.toLowerCase() === filter) ||
+            recipe.appliance.toLowerCase() === filter ||
+            recipe.ustensils.some((ustensil) => ustensil.toLowerCase() === filter)
+        )
     )
 }
 
@@ -300,7 +233,6 @@ function updateRecipesDisplay(data) {
             updateRecipeCount(recipesToUse)
             displayedRecipes = recipesToUse
         }
-
     }
 }
 
@@ -328,7 +260,6 @@ function handleSearchbarInput() {
             updateRecipeCount(searchedRecipes)
         }
     } else {
-    } else {
         updateRecipesDisplay()
         searchedRecipes = []
         displayedRecipes = activeFilters.length > 0 ? filteredRecipes : recipesData
@@ -338,7 +269,6 @@ function handleSearchbarInput() {
 
 function displayNoResultsMessage(searchTerm) {
     const recipesContainer = document.querySelector(".cards_section")
-    recipesContainer.innerHTML = `<p class="text-center col-start-2 my-[80px]">Aucune recette ne contient "${searchTerm}", vous pouvez chercher "tarte aux pommes", "poisson", etc.</p>`
     recipesContainer.innerHTML = `<p class="text-center col-start-2 my-[80px]">Aucune recette ne contient "${searchTerm}", vous pouvez chercher "tarte aux pommes", "poisson", etc.</p>`
 }
 
